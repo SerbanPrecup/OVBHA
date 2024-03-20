9e0efa678aa738fd85356a2e1e5bb140926ce94b
SerbanPrecup <precup.serban@yahoo.com>
2024-03-20 15:25:56 +0200

    Am reusit sa generez harta+locatia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ secrets {
 
     // A properties file containing default secret values. This file can be
     // checked in version control.
-    defaultPropertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
 
     // Configure which keys should be ignored by the plugin by providing regular expressions.
     // "sdk.dir" is ignored by default.
